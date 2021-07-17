65cb3952e91eab7c12733194bfaa9329b1d47ada
AlexandrDorosh <dovgaloleksandr388@gmail.com>
2021-07-17 20:41:16 +0300

    finish hw 7

--- src/components/forms/forms.js
+++ src/components/forms/forms.js
@@ -7,10 +7,10 @@ export default function Forms({users}){
 
     const userChoose = (e) => {
         const user_id = e.target.value;
-        const find = users.find(user => user.id === user_id)
-        setUser(find);
-        const find2 = users.find(user => user.id === +user_id)
-        setUserInfo(find2);
+        const findUser = users.find(user => user.id === user_id)
+        setUser(findUser);
+        const findInfo = users.find(user => user.id === +user_id)
+        setUserInfo(findInfo);
     }
 
     return(
