cb4b9a769cb3edcc18b285b961f4e067aa516d05
AlexandrDorosh <dovgaloleksandr388@gmail.com>
2021-07-26 15:53:40 +0300

    fixed

--- src/components/forms/forms.js
+++ src/components/forms/forms.js
@@ -2,7 +2,7 @@ import {useState} from "react";
 import Info from "../info/info";
 
 export default function Forms({users}){
-    const [user, setUser] = useState(undefined);
+    const [user, setUser] = useState(null);
 
     const userChoose = (e) => {
         const findInfo = users.find(user => user.id === +e.target.value)
@@ -12,7 +12,8 @@ export default function Forms({users}){
     return(
         <div>
             <form onSubmit={userChoose}>
-                <select onChange={userChoose} value={user}>
+                <select onChange={userChoose}>
+                    <option selected disabled hidden>Choose here</option>
                     {
                         users.map(value => <option key={value.id} value={value.id}>{value.name}</option>)
                     }
